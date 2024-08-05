8c99e2baa9c97a44da46176e667e1c7c13d156d5
Abdur Razzaq <abdurrazaq789@gmail.com>
2024-08-05 10:50:21 +0500

    for test

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About</a></li>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
